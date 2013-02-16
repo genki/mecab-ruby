cfc6ee47dca70121114623ae538777708fe58220
Genki Takiuchi <genki@s21g.com>
2013-02-16 11:54:21 +0900

    Added ruby lib file

---
+++ lib/mecab-ruby.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), %w[.. MeCab])
+
+module MeCab
+  class Nodes
+    include Enumerable
+
+    def initialize(nodes)
+      @nodes = nodes
+    end
+
+    def each(&block)
+      node = @nodes
+      while node do
+        block.call node
+        node = node.next
+      end
+    end
+  end
+
+  class Tagger
+    def parse_to_nodes(text)
+      Nodes.new parseToNode(text)
+    end
+  end
+end
